74796856457af6b5f136a0acd41c4ccf0660ce0b
Matthew Coal <mate.coalie@gmail.com>
2023-10-13 22:47:30 +0200

    fix(types): proper typings for the module & lib

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Include git information in your Vite builds.
 ## Example usage
 
 vite.config.js
+
 ```js
 import { defineConfig } from "vite";
 import gitInfoPlugin from "vite-plugin-add-gitinfo";
@@ -15,7 +16,9 @@ export default defineConfig({
 ```
 
 myfile.ts
+
 ```ts
+/// <reference types="vite-plugin-add-gitinfo" />
 import gitInfo from "virtual:gitinfo";
 
 console.log(gitInfo.sha); // 123abcd
--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,3 @@
+import "./module";
+
 export default function addGitInfo(): import("vite").Plugin;
-
-declare module "virtual:gitinfo" {
-  const gitInfo: {
-    /**
-     * Commit hash resolved from HEAD.
-     */
-    sha: string;
-
-    /**
-     * The current checked out branch.
-     */
-    branch: string;
-
-    /**
-     * The date the commit was authored. (ISO 8601 format)
-     */
-    committedAt: string;
-  };
-
-  export default gitInfo;
-}
